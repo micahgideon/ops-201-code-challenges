e6c7650f9522ad05c4d56c984d8737fe2b0a696d
Micah Miranda <micahgideon@yahoo.com>
2023-02-08 20:01:55 -0800

    Ops Challenge - Functions

---
+++ functions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Micah	Miranda
+# Date of latest revision: 2/8/23
+# Purpose: Print a string to the terminal
+
+# Main
+
+print_logins () {
+echo | last
+}
+
+print_logins
+
+# End
