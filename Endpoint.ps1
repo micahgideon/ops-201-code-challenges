bf401ce7065747e8d79fbb8f4fa56526ab8f5c4e
Micah Miranda <micahgideon@yahoo.com>
2023-02-21 04:48:10 +0000

    Endpoints

---
+++ Endpoint.ps1
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 11 Ops Challenge Solution
+# Author: Micah	Miranda
+# Date of latest revision: 2/20/23
+# Purpose: Create a script that automates Windows system configurations
+
+# Source: https://github.com/superswan/Powershell-SysAdmin
+
+# Main
+
+# Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+
+# Enable ICMP Traffic
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4 
+
+# Enable Remote management
+# RDP
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+# Network level Authentication
+Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\‘ -Name “UserAuthentication” -Value 1
+# Firewall Rule
+Enable-NetFirewallRule -DisplayGroup “Remote Desktop”
+
+#Bloatware Remover
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+#Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+# Disable SMBv1, an insecure protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+
+# End
