a89e84f7d1b3fc91bcaca81faff902c605655be1
Micah Miranda <micahgideon@yahoo.com>
2023-02-22 18:00:44 +0000

    IPanalysis2

--- IPAnalysis.ps1
+++ IPAnalysis.ps1
@@ -8,9 +8,9 @@ $path = "network_report.txt"
 # Create a local file called network_report.txt that holds the contents of an ipconfig /all command.
 function GetIPAddress {ipconfig /all > $path
 # Use Select-String to search network_report.txt and return only the IP version 4 address.
-	$ipAddress = Get-Content network_report.txt | Select-String -Context 0,31 -Pattern 'Ethernet adapter Ethernet' | Select-Object -ExpandProperty Context | Select-String -Pattern '\bIPv4 Address\b.*(\d{1,3}\.){3}\d{1,3}\b' -Context 0,7
+	$ipAddress =  Select-String -Path $path -Pattern '192.168.0.\d{1,3}'
 # Remove the network_report.txt when you are finished searching it.
-	Remove-Item -Path
+	Remove-Item -Path $path
 
 	return ipAddress
 }
