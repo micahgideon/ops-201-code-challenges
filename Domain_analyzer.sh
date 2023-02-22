03692cdf2869c4fd822b859ef4acfd594fc23226
Micah Miranda <micahgideon@yahoo.com>
2023-02-22 20:57:27 +0000

    domain

---
+++ Domain_analyzer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 13 Ops Challenge Solution
+# Author: Micah	Miranda
+# Date of latest revision: 2/22/23
+# Purpose: Detect is a file or directory exists, then creats one if it doesn't
+
+# Main
+
+domain_information () {
+	# Take a user input string. Presumably the string is a domain name such as Google.com.
+	echo "Enter a domain name:"
+	read domain
+
+	# Run whois against a user input string.
+	whois $domain >> domain.txt
+	dig $domain >> domain.txt
+	host $domain >> domain.txt
+	nslookup $domain >> domain.txt
+}
+
+domain_information
+
+code domain.txt
+
+# End
