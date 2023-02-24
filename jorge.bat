a9f90257d438b1f46e02c4a2facdc538d9daa3ff
Micah Miranda <micahgideon@yahoo.com>
2023-02-24 17:50:25 +0000

    jorge2

--- jorge.bat
+++ jorge.bat
@@ -1,1 +1,7 @@
+:: Script: Ops 201 Class 08 Ops Challenge Solution
+:: Author: Micah Miranda
+:: Date of latest revision: 2/24/23
+:: Purpose: Write a batch script that coppies files
+
+:: Create a batch file that recursively copies Jorge’s work files on his desktop (recursively meaning to include folder contents).
 robocopy C:\users\j.thompson\Desktop C:\users\j.thompson\folder\ /E
